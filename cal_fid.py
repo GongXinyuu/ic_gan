4d183d2607b0f75fb977759c163f4de318161b69
neogong <xinyu.gong@utexas.edu>
2022-04-04 19:03:22 -0500

    refactor cal_fid

--- cal_fid.py
+++ cal_fid.py
@@ -6,37 +6,36 @@
 
 import os
 import sys
-from cleanfid.fid import compute_fid
+import warnings
 
+import cma
+import imageio
 import numpy as np
 import scipy  # noqa
 import torch
+import torchvision.transforms as transforms
+from cleanfid.fid import compute_fid
+from imageio import imsave
+from PIL import Image as Image_PIL
 from pytorch_pretrained_biggan import (
     convert_to_images,
 )
 from scipy.stats import truncnorm
+from torch import nn
 from tqdm import tqdm
 
-torch.manual_seed(np.random.randint(sys.maxsize))
-import imageio
-from PIL import Image as Image_PIL
-from torch import nn
-import cma
-import warnings
+import data_utils.utils as data_utils
+import inference.utils as inference_utils
+from config.config import get_cfg
+from config.parser import parse_args
+from data.build import build_loader
 
 warnings.simplefilter("ignore", cma.evolution_strategy.InjectionWarning)
-import torchvision.transforms as transforms
-import inference.utils as inference_utils
-import data_utils.utils as data_utils
+torch.manual_seed(np.random.randint(sys.maxsize))
 
-# import sklearn.metrics
-from config.parser import parse_args
 
-from config.config import get_cfg
-from data.build import build_loader
-from imageio import imsave
-
-load_root = "pretrained_models_path"
+norm_mean = torch.Tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
+norm_std = torch.Tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
 
 
 def denormalize_img(torch_img: torch.Tensor) -> torch.Tensor:
@@ -45,14 +44,6 @@ def denormalize_img(torch_img: torch.Tensor) -> torch.Tensor:
     :param torch_img: N x C x H x W.
     :return: np.ndarray (range: 0 ~ 255)
     """
-    # return (
-    #     torch_img.mul_(127.5)
-    #     .add_(127.5)
-    #     .clamp_(0.0, 255.0)
-    #     .permute(0, 2, 3, 1)
-    #     .to("cpu", torch.uint8)
-    #     .numpy()
-    # )
     return torch_img.mul_(127.5).add_(127.5).clamp_(0.0, 255.0)
 
 
@@ -108,99 +99,109 @@ def check_cfg(cfg):
     assert len(cfg.MODEL.LOSS.HYPER_REG.TYPE) == len(cfg.MODEL.LOSS.HYPER_REG.CONFIG)
 
 
-def main(args, save):
-    def load_icgan(experiment_name, root_="/content"):
-        root = os.path.join(root_, experiment_name)
-        config = torch.load("%s/%s.pth" % (root, "state_dict_best0"))["config"]
+def load_icgan(experiment_name, root_="/content"):
+    root = os.path.join(root_, experiment_name)
+    config = torch.load("%s/%s.pth" % (root, "state_dict_best0"))["config"]
 
-        config["weights_root"] = root_
-        config["model_backbone"] = "biggan"
-        config["experiment_name"] = experiment_name
-        G, config = inference_utils.load_model_inference(config)
-        G.cuda()
-        G.eval()
-        return G
+    config["weights_root"] = root_
+    config["model_backbone"] = "biggan"
+    config["experiment_name"] = experiment_name
+    G, config = inference_utils.load_model_inference(config)
+    G.cuda()
+    G.eval()
+    return G
 
-    def get_output(noise_vector, input_label, input_features):
-        if stochastic_truncation:  # https://arxiv.org/abs/1702.04782
-            with torch.no_grad():
-                trunc_indices = noise_vector.abs() > 2 * truncation
-                size = torch.count_nonzero(trunc_indices).cpu().numpy()
-                trunc = truncnorm.rvs(
-                    -2 * truncation, 2 * truncation, size=(1, size)
-                ).astype(np.float32)
-                noise_vector.data[trunc_indices] = torch.tensor(
-                    trunc, requires_grad=False, device="cuda"
-                )
-        else:
-            noise_vector = noise_vector.clamp(-2 * truncation, 2 * truncation)
-        if input_label is not None:
-            input_label = torch.LongTensor(input_label)
-        else:
-            input_label = None
 
-        out = model(
-            noise_vector,
-            input_label.cuda() if input_label is not None else None,
-            input_features.cuda() if input_features is not None else None,
-        )
-
-        if channels == 1:
-            out = out.mean(dim=1, keepdim=True)
-            out = out.repeat(1, 3, 1, 1)
-        return out
-
-    def load_generative_model(gen_model, last_gen_model, experiment_name, model):
-        # Load generative model
-        if gen_model != last_gen_model:
-            model = load_icgan(experiment_name, root_=load_root)
-            last_gen_model = gen_model
-        return model, last_gen_model
-
-    def load_feature_extractor(gen_model, last_feature_extractor, feature_extractor):
-        # Load feature extractor to obtain instance features
-        feat_ext_name = (
-            "classification" if gen_model == "cc_icgan" else "selfsupervised"
-        )
-        if last_feature_extractor != feat_ext_name:
-            if feat_ext_name == "classification":
-                feat_ext_path = ""
-            else:
-                # !curl - L - o / content / swav_pretrained.pth.tar - C - 'https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar'
-                feat_ext_path = "pretrained_models_path/swav_pretrained.pth.tar"
-            last_feature_extractor = feat_ext_name
-            feature_extractor = data_utils.load_pretrained_feature_extractor(
-                feat_ext_path, feature_extractor=feat_ext_name
+def get_output(
+    noise_vector,
+    input_label,
+    input_features,
+    stochastic_truncation,
+    truncation,
+    model,
+    channels,
+):
+    if stochastic_truncation:  # https://arxiv.org/abs/1702.04782
+        with torch.no_grad():
+            trunc_indices = noise_vector.abs() > 2 * truncation
+            size = torch.count_nonzero(trunc_indices).cpu().numpy()
+            trunc = truncnorm.rvs(
+                -2 * truncation, 2 * truncation, size=(1, size)
+            ).astype(np.float32)
+            noise_vector.data[trunc_indices] = torch.tensor(
+                trunc, requires_grad=False, device="cuda"
             )
-            feature_extractor.eval()
-        return feature_extractor, last_feature_extractor
+    else:
+        noise_vector = noise_vector.clamp(-2 * truncation, 2 * truncation)
+    if input_label is not None:
+        input_label = torch.LongTensor(input_label)
+    else:
+        input_label = None
 
-    norm_mean = torch.Tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-    norm_std = torch.Tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
+    out = model(
+        noise_vector,
+        input_label.cuda() if input_label is not None else None,
+        input_features.cuda() if input_features is not None else None,
+    )
 
-    def preprocess_input_image(input_image_path, size):
-        pil_image = Image_PIL.open(input_image_path).convert("RGB")
-        transform_list = transforms.Compose(
-            [
-                data_utils.CenterCropLongEdge(),
-                transforms.Resize((size, size)),
-                transforms.ToTensor(),
-                transforms.Normalize(norm_mean, norm_std),
-            ]
-        )
-        tensor_image = transform_list(pil_image)
-        tensor_image = torch.nn.functional.interpolate(
-            tensor_image.unsqueeze(0), 224, mode="bicubic", align_corners=True
-        )
-        return tensor_image
+    if channels == 1:
+        out = out.mean(dim=1, keepdim=True)
+        out = out.repeat(1, 3, 1, 1)
+    return out
 
-    def preprocess_generated_image(image):
-        transform_list = transforms.Normalize(norm_mean, norm_std)
-        image = transform_list(image * 0.5 + 0.5)
-        image = torch.nn.functional.interpolate(
-            image, 224, mode="bicubic", align_corners=True
+
+def load_generative_model(gen_model, last_gen_model, experiment_name, model, load_root):
+    # Load generative model
+    if gen_model != last_gen_model:
+        model = load_icgan(experiment_name, root_=load_root)
+        last_gen_model = gen_model
+    return model, last_gen_model
+
+
+def load_feature_extractor(gen_model, last_feature_extractor, feature_extractor):
+    # Load feature extractor to obtain instance features
+    feat_ext_name = "classification" if gen_model == "cc_icgan" else "selfsupervised"
+    if last_feature_extractor != feat_ext_name:
+        if feat_ext_name == "classification":
+            feat_ext_path = ""
+        else:
+            # !curl - L - o / content / swav_pretrained.pth.tar - C - 'https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar'
+            feat_ext_path = "pretrained_models_path/swav_pretrained.pth.tar"
+        last_feature_extractor = feat_ext_name
+        feature_extractor = data_utils.load_pretrained_feature_extractor(
+            feat_ext_path, feature_extractor=feat_ext_name
         )
-        return image
+        feature_extractor.eval()
+    return feature_extractor, last_feature_extractor
+
+
+def preprocess_input_image(input_image_path, size):
+    pil_image = Image_PIL.open(input_image_path).convert("RGB")
+    transform_list = transforms.Compose(
+        [
+            data_utils.CenterCropLongEdge(),
+            transforms.Resize((size, size)),
+            transforms.ToTensor(),
+            transforms.Normalize(norm_mean, norm_std),
+        ]
+    )
+    tensor_image = transform_list(pil_image)
+    tensor_image = torch.nn.functional.interpolate(
+        tensor_image.unsqueeze(0), 224, mode="bicubic", align_corners=True
+    )
+    return tensor_image
+
+
+def preprocess_generated_image(image):
+    transform_list = transforms.Normalize(norm_mean, norm_std)
+    image = transform_list(image * 0.5 + 0.5)
+    image = torch.nn.functional.interpolate(
+        image, 224, mode="bicubic", align_corners=True
+    )
+    return image
+
+
+def main(args, save, load_root, experiment_name=None):
 
     cfg = setup(args)
     last_gen_model = None
@@ -216,11 +217,11 @@ def main(args, save):
     # else:
     #     experiment_name = f"cc_icgan_biggan_imagenet_res{size}"
 
-    if gen_model == "icgan":
-        experiment_name = f"icgan_biggan_{dataset}_res{size}"
-    else:
-        experiment_name = f"cc_icgan_biggan_{dataset}_res{size}"
-    # last_gen_model = experiment_name
+    if experiment_name is None:
+        if gen_model == "icgan":
+            experiment_name = f"icgan_biggan_{dataset}_res{size}"
+        else:
+            experiment_name = f"cc_icgan_biggan_{dataset}_res{size}"
 
     input_image_instance = "cat.JPG"  # @param {type:"string"}
     input_feature_index = 3  # @param {type:'integer'}
@@ -258,7 +259,7 @@ def main(args, save):
 
     # Load generative model
     model, last_gen_model = load_generative_model(
-        gen_model, last_gen_model, experiment_name, model
+        gen_model, last_gen_model, experiment_name, model, load_root
     )
     # Prepare other variables
     name_file = "%s_class_index%s_instance_index%s" % (
@@ -338,10 +339,11 @@ def main(args, save):
                 noise_vector[start:end],
                 input_label[start:end] if input_label is not None else None,
                 cur_instance_vector.cuda() if cur_instance_vector is not None else None,
+                stochastic_truncation,
+                truncation,
+                model,
+                channels,
             )
-            # import ipdb
-            #
-            # ipdb.set_trace()
 
             all_outs.extend(list(numpy_img(denormalize_img(out))))
             # all_outs.append(out.detach().cpu())
@@ -367,4 +369,6 @@ if __name__ == "__main__":
     save = False
     args = parse_args()
     print("Command Line Args:", args)
-    main(args, save)
+    load_root = "pretrained_models_path"
+    experiment_name = None
+    main(args, save, load_root=load_root, experiment_name=experiment_name)
